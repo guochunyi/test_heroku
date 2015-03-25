9b97c5bc858ec27c569606084c783799738ebf82
guo_chunyi <chunyi.guo@drecom.co.jp>
2015-03-25 14:51:28 +0900

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FirstTest::Application.routes.draw do
+  get "static_pages/help"
+  get "static_pages/home"
+  root :to => 'static_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
