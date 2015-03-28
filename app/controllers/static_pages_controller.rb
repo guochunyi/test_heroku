f2c47162083bf2908346a24563e9e407a9751033
guo_chunyi <chunyi.guo@drecom.co.jp>
2015-03-28 16:13:48 +0900

    统计用户bug修复

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    track_user(request.remote_addr, RedisHelper::UserType::ANONY)
+    if signed_in?
+      track_user(request.remote_addr, RedisHelper::UserType::LOGIN)
+    else
+      track_user(request.remote_addr, RedisHelper::UserType::ANONY)
+    end
   end
 
   def help
